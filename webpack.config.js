b7a4c96453b85d2c2b77d932b4a4a235f2207c67
morlucho <luis-fernando-10@outlook.es>
2022-02-05 00:59:35 -0500

    [Deploy]4 deleted / at filename

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: '/ReactShop/bundle.js',
+		filename: 'ReactShop/bundle.js',
 		publicPath: '/ReactShop/'
 	},
 	mode: 'development',
