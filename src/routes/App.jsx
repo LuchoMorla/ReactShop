04c999c9101c4b2f1978ac609ea37525dd0fe406
morlucho <luis-fernando-10@outlook.es>
2022-02-05 01:44:50 -0500

    Page Home contain routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -22,16 +22,17 @@ const App = () => {
 		<BrowserRouter>
 			<Layout>
 				<Routes>
-					<Route exact path="/" element={<Home />} />
-					<Route exact path="/login" element={<Login />} />
-					<Route exact path="/password-recovery" element={<PasswordRecovery />} />
-					<Route exact path="/send-email" element={<SendEmail />} />
-					<Route exact path="/new-password" element={<NewPassword />} />
-					<Route exact path="/account" element={<MyAccount />} />
-					<Route exact path="/signup" element={<CreateAccount />} />
-					<Route exact path="/checkout" element={<Checkout />} />
-					<Route exact path="/orders" element={<Orders />} />
-					<Route path="*" element={<NotFound />} />
+					<Route exact path="/" element={<Home />} >
+						<Route exact path="/login" element={<Login />} />
+						<Route exact path="/password-recovery" element={<PasswordRecovery />} />
+						<Route exact path="/send-email" element={<SendEmail />} />
+						<Route exact path="/new-password" element={<NewPassword />} />
+						<Route exact path="/account" element={<MyAccount />} />
+						<Route exact path="/signup" element={<CreateAccount />} />
+						<Route exact path="/checkout" element={<Checkout />} />
+						<Route exact path="/orders" element={<Orders />} />
+						<Route path="*" element={<NotFound />} />
+					</Route>
 				</Routes>
 			</Layout>
 		</BrowserRouter>
